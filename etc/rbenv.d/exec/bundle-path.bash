9f825cfdb7df252d603ef8fc2ca19441ddc96cb5
Justice London <justice@ministrycentered.com>
2015-08-13 23:49:43 -0700

    The default path needs to map to persist folder.

--- etc/rbenv.d/exec/bundle-path.bash
+++ etc/rbenv.d/exec/bundle-path.bash
@@ -3,5 +3,5 @@ ruby_abi=$( echo "$RBENV_VERSION" | cut -d. -f1-2 ).0
 if ! [ -z "$BUNDLE_OVERRIDE_ROOT" ]; then
   export BUNDLE_PATH="$BUNDLE_OVERRIDE_ROOT/ruby/$ruby_abi"
 else
-  export BUNDLE_PATH=".bundle/ruby/$ruby_abi"
+  export BUNDLE_PATH="/opt/persist/ruby/ruby/$ruby_abi"
 fi
