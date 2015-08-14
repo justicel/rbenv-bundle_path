7af1fad5488f9828a049ff21a19a00caee6038c2
Justice London <justice@ministrycentered.com>
2015-08-13 21:52:20 -0700

    Whoops. Can't check variable directly.

--- etc/rbenv.d/exec/bundle-path.bash
+++ etc/rbenv.d/exec/bundle-path.bash
@@ -1,6 +1,6 @@
 ruby_abi=$( echo "$RBENV_VERSION" | cut -d. -f1-2 ).0
 
-if $BUNDLE_OVERRIDE_ROOT; then
+if ! [ -z "$BUNDLE_OVERRIDE_ROOT" ]; then
   export BUNDLE_PATH="$BUNDLE_OVERRIDE_ROOT/ruby/$ruby_abi"
 else
   export BUNDLE_PATH=".bundle/ruby/$ruby_abi"
