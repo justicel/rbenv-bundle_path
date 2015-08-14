07a894ce30deccb28a01827036cf3ecf5abe078a
Justice London <justice@ministrycentered.com>
2015-08-13 21:45:52 -0700

    Added some additional logic to allow override of bundle-override path

--- etc/rbenv.d/exec/bundle-path.bash
+++ etc/rbenv.d/exec/bundle-path.bash
@@ -1,2 +1,7 @@
 ruby_abi=$( echo "$RBENV_VERSION" | cut -d. -f1-2 ).0
-export BUNDLE_PATH=".bundle/ruby/$ruby_abi"
+
+if $BUNDLE_OVERRIDE_ROOT; then
+  export BUNDLE_PATH="$BUNDLE_OVERRIDE_ROOT/ruby/$ruby_abi"
+else
+  export BUNDLE_PATH=".bundle/ruby/$ruby_abi"
+fi
